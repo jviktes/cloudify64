b28b8aaa45eef51031dcb875a09cac5cc19e09fd
Pavel <jviktes@gmail.com>
2023-03-16 19:58:50 +0100

    disk label according os

--- widgets/virtualMachines/src/DataDisksTableVM.tsx
+++ widgets/virtualMachines/src/DataDisksTableVM.tsx
@@ -101,6 +101,15 @@ export default class DataDisksTableVM extends React.Component<DataDisksTableVMPr
         let _extraData = "Host caching: "+item.host_caching + "Disk label: " + item.label;
         return _extraData;
     }
+    getTableLabelForMountPoint = (_vmData:any) => {
+        if (_vmData?.os.indexOf("Windows")!=-1) {
+            return ("Disk letter");
+        }
+        else {
+            return ("Mount point");
+        }
+    };
+
     render() {
         /* eslint-disable no-console, no-process-exit */
         const { data, toolbox, widget,vmData } = this.props;
@@ -127,7 +136,7 @@ export default class DataDisksTableVM extends React.Component<DataDisksTableVMPr
 
                 <DataTable className="" noDataMessage="There are no data disks">
                     <DataTable.Column label="Name" name="disk_name"/>
-                    <DataTable.Column label="Mountpoint/Disk letter" name="mountpoint"/>
+                    <DataTable.Column label={this.getTableLabelForMountPoint(vmData)} name="mountpoint"/>
 
                     <DataTable.Column label="LUN" name="lun"/>
                     <DataTable.Column label="Disk type" name="disk_type" />
