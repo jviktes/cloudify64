8e451e4d33b2b6a55e737de65ec8caf54d6dc610
Pavel <jviktes@gmail.com>
2023-03-30 15:09:51 +0200

    add parsing cpu and ram

--- widgets/virtualMachines/src/VMHelpers.tsx
+++ widgets/virtualMachines/src/VMHelpers.tsx
@@ -1,3 +1,4 @@
+
 export function getDetails (item:any){
         
     let _display_name = "";
@@ -20,31 +21,35 @@ export function getDetails (item:any){
                 _azure_location=_getLocation(_inputsCapabilities);
                 _environment=_getEnvironment(_inputsCapabilities);
                 //TODO:
-                _cpus=_getCPU(_inputsCapabilities);
-                _ram=_getRAM(_inputsCapabilities);
+                _cpus=_getCPU(item, _azure_size);
+                _ram=_getRAM(item,_azure_size);
 
                 _display_name = item.id;
             }
 
-            let _index = -1;
+            //let _index = -1;
             if (item.executionAllData[0].items!=null){
                 //hledani indexu ve vm, v execution pro create
-                for (let index = 0; index < item.executionAllData[0].items.length; index++) {
-                    const element = item.executionAllData[0].items[index];
-                    if (element.workflow_id=="create_deployment_environment") {
-                        _index=index;
-                        break;
-                    }
-                }
+                // for (let index = 0; index < item.executionAllData[0].items.length; index++) {
+                //     const element = item.executionAllData[0].items[index];
+                //     if (element.workflow_id=="create_deployment_environment") {
+                //         _index=index;
+                //         break;
+                //     }
+                // }
 
-                if (_index!=-1) {
-                    let _inputs = item.executionAllData[0].items[_index]["parameters"]["inputs"];
-                     _cpus=_getCPU(_inputs);
-                     _ram=_getRAM(_inputs);
-                    _os=_getOS(_inputs);
-                }
+                  var _workflow = item.executionAllData[0].items.filter(obj => {
+                    return obj.workflow_id === "create_deployment_environment"
+                  })
 
-
+                let _inputs = _workflow[0]["parameters"]["inputs"];
+                _os=_getOS(_inputs);
+                // if (_index!=-1) {
+                //     let _inputs = item.executionAllData[0].items[_index]["parameters"]["inputs"];
+                //      //_cpus=_getCPU(_inputs);
+                //      //_ram=_getRAM(_inputs);
+                //     _os=_getOS(_inputs);
+                // }
             }
 
     } catch (error) {
@@ -76,20 +81,60 @@ export function _getIPAdress (inputJson:any){
         return "";
     }
 }
-export function _getCPU (inputJson:any){
+export function _getCPU (deployment:any,_azure_size:any){
     try {
-        return inputJson["size"]["cpu"];
+
+        var _workflow = deployment.workflows.filter((obj: { name: string; }) => {
+            return obj.name === "resize_windows_vm"
+          })
+        //console.log(vm_size); //(2 CPU, 8GB RAM, max 4 data disks)
+        let _list_of_azure_sizes = _workflow[0].parameters.size.constraints[0].valid_values;
+
+        let _index = -1;  
+        for (let index = 0; index < _list_of_azure_sizes.length; index++) {
+            const element = _list_of_azure_sizes[index];
+            if (element.includes(_azure_size)) {
+                _index=index;
+                break;
+            }
+        }
+
+        var  _vm_size_description = _list_of_azure_sizes[_index];
+        let cpu = _vm_size_description.substring(_vm_size_description.indexOf("(")+1,_vm_size_description.indexOf("CPU"));
+        cpu = cpu.replace(' ', '');
+        return cpu;
     } catch (error) {
         return "";
     }
 }
-export function _getRAM (inputJson:any){
+
+export function _getRAM (deployment:any,_azure_size:any){
     try {
-        return inputJson["size"]["ram"];
+
+        var _workflow = deployment.workflows.filter((obj: { name: string; }) => {
+            return obj.name === "resize_windows_vm"
+          })
+          let _list_of_azure_sizes = _workflow[0].parameters.size.constraints[0].valid_values;
+
+          let _index = -1;  
+          for (let index = 0; index < _list_of_azure_sizes.length; index++) {
+              const element = _list_of_azure_sizes[index];
+              if (element.includes(_azure_size)) {
+                  _index=index;
+                  break;
+              }
+          }
+  
+          var  _vm_size_description = _list_of_azure_sizes[_index];
+        let _ram = _vm_size_description.substring(_vm_size_description.indexOf(",")+1,_vm_size_description.indexOf("RAM"));
+        _ram = _ram.replace('GB', '').replace(' ', '');
+        //console.log(vm_size); //(2 CPU, 8GB RAM, max 4 data disks)
+        return _ram;
     } catch (error) {
         return "";
     }
 }
+
 export function _getAzureSize (inputJson:any){
     try {
         //return inputJson["size"]["id"];
