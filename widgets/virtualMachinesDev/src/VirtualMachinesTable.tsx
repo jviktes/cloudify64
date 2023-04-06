1e0e82071d077b55fd46708ebcbcd3794c41bdd8
Pavel <jviktes@gmail.com>
2023-04-06 09:06:32 +0200

    sorting by capabilities column

--- widgets/virtualMachinesDev/src/VirtualMachinesTable.tsx
+++ widgets/virtualMachinesDev/src/VirtualMachinesTable.tsx
@@ -354,13 +354,13 @@ export default class VirtualMachinesTable extends React.Component<VirtualMachine
                 >
 
                     <DataTable.Column label="Name" name="display_name"/>
-                    <DataTable.Column label="OS" name="os" />
+                    <DataTable.Column label="OS" name="capabilities" />
                     <DataTable.Column label="IP" name="capabilities" />
-                    <DataTable.Column label="CPUs" name="cpus" />
-                    <DataTable.Column label="RAM (GiB)" name="ram" />
-                    <DataTable.Column label="Azure size" name="azure_size" />
-                    <DataTable.Column label="Azure location" name="azure_location" />
-                    <DataTable.Column label="Environment" name="environment" />
+                    <DataTable.Column label="CPUs" name="capabilities" />
+                    <DataTable.Column label="RAM (GiB)" name="capabilities" />
+                    <DataTable.Column label="Azure size" name="capabilities" />
+                    <DataTable.Column label="Azure location" name="capabilities" />
+                    <DataTable.Column label="Environment" name="capabilities" />
                     <DataTable.Column label="" name="" />
                     <DataTable.Column label="Actions" />
                     {/* <DataTable.Column label="" name="config" name="class" /> */}
