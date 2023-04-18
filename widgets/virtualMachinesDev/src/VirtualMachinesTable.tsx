5b48807065ca3823eb62108730bac9361f257340
Pavel <jviktes@gmail.com>
2023-04-18 10:00:10 +0200

    up version

--- widgets/virtualMachinesDev/src/VirtualMachinesTable.tsx
+++ widgets/virtualMachinesDev/src/VirtualMachinesTable.tsx
@@ -428,7 +428,7 @@ export default class VirtualMachinesTable extends React.Component<VirtualMachine
 
         return (
             <div>
-                <span style={{float:"right",fontSize:"smaller"}}>Version:1.40</span>
+                <span style={{float:"right",fontSize:"smaller"}}>Version:1.41</span>
                 <DataTable
                     className="table-scroll-vm"
                     pageSize={widget.configuration.pageSize}
